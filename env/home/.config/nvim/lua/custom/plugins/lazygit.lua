d2c24258bf5c2b3028b2c3ff6ad8f309dce05f90
pegov <dvpegov@gmail.com>
2025-06-03 22:46:35 +0300

    [nvim] add general floating terminal

--- env/home/.config/nvim/lua/custom/plugins/lazygit.lua
+++ env/home/.config/nvim/lua/custom/plugins/lazygit.lua
@@ -14,36 +14,73 @@ return {
 		"akinsho/toggleterm.nvim",
 	},
 	config = function()
-		local term = nil
+		local lg_term = nil
+		local ge_term = nil
+
+		local t = require("toggleterm.terminal").Terminal
+
+		local size = 90
+
+		local size_w_f = function()
+			return vim.o.columns * 0.95
+		end
+		local size_h_f = function()
+			return vim.o.lines * 0.9
+		end
+
+		local direction = "float"
 
 		local function lg_toggle()
-			local Terminal = require("toggleterm.terminal").Terminal
-
-			local size = 90
-			local direction = "float"
-
-			if not term then
-				term = Terminal:new({
+			if not lg_term then
+				lg_term = t:new({
 					cmd = "lazygit",
 					hidden = true,
 					on_exit = function()
-						term = nil
+						lg_term = nil
 					end,
 				})
-				if term then
-					term:toggle(size, direction)
+				if lg_term then
+					lg_term:toggle(size(), direction)
 
 					vim.cmd("set ft=lazygit")
 					vim.keymap.set("t", "<a-q>", function()
-						term:toggle(size, direction)
+						lg_term:toggle(size(), direction)
 					end, { buffer = true })
 				end
 			else
-				term:toggle(size, direction)
+				lg_term:toggle(size, direction)
+			end
+		end
+
+		local function ge_term_toggle()
+			if not ge_term then
+				ge_term = t:new({
+					float_opts = {
+						width = math.ceil(size_w_f()),
+						height = math.ceil(size_h_f()),
+					},
+					hidden = true,
+					on_exit = function()
+						ge_term = nil
+					end,
+				})
+				if ge_term then
+					ge_term:toggle(size, direction)
+
+					vim.cmd("set ft=general_terminal")
+					vim.keymap.set("t", "<F2>", function()
+						ge_term:toggle(size, direction)
+					end)
+				end
+			else
+				ge_term:toggle(size, direction)
 			end
 		end
 
 		vim.api.nvim_create_user_command("LazyGitToggle", lg_toggle, {})
 		vim.keymap.set("n", "<leader><leader>l", "<cmd>LazyGitToggle<cr>", {})
+
+		vim.api.nvim_create_user_command("GeneralTermToggle", ge_term_toggle, {})
+		vim.keymap.set("n", "<F2>", "<cmd>GeneralTermToggle<cr>", {})
 	end,
 }
